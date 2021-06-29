4103a0261127677705025171aef51ea19d8b9d82
Magnus Larsson <zpeedi@hotmail.com>
2021-06-29 10:55:54 +0200

    Person and tests done.

---
+++ TodoIT.Tests/UnitTest1.cs
@@ -0,0 +1,41 @@
+using System;
+using Xunit;
+using TodoIT.Model;
+
+
+
+namespace TodoIT.Tests
+{
+    public class PersonTests
+    {
+        [Fact]
+        public void IdCounter()
+        {
+            string firstname = "Magnus";
+            string lastname = "Larsson";
+            int befopreId = Person.IdCounter;
+            Person testPerson = new Person(firstname, lastname);
+            
+            Assert.Equal(testPerson.Firstname, firstname);
+            Assert.Equal(testPerson.Lastname, lastname);
+            Assert.True(Person.IdCounter > befopreId);
+        }
+
+        [Fact]
+        public void PersonId()
+        {
+            Person testPerson1 = new Person("Magnus", "Larsson");
+            Person testPerson2 = new Person("Kent", "Larsson");
+            Assert.NotEqual(testPerson1.PersonId, testPerson2.PersonId);
+        }
+
+        [Fact]
+        public void Arguementexception()
+        {
+            Person testPerson1;
+
+            Assert.Throws<ArgumentException>(() => testPerson1 = new Person("", "Larsson"));
+            Assert.Throws<ArgumentException>(() => testPerson1 = new Person(null, "Larsson"));
+        }
+    }
+}
---
+++ TodoIT/Model/Person.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TodoIT.Model
+{
+    public class Person
+    {
+        private static int idCounter = 0;
+        private readonly int personId;
+        private string firstname;
+        private string lastname;
+
+        public Person(string firstname, string lastname)
+        {
+            personId = ++idCounter;
+            Firstname = firstname;
+            Lastname = lastname;
+        }
+
+        public int PersonId
+        {
+            get { return personId; }
+        }
+        public static int IdCounter
+        {
+            get { return idCounter; }
+        }
+
+        public string Firstname
+        {
+            get { return firstname; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException();
+                }
+                firstname = value;
+            }
+        }
+
+        public string Lastname
+        {
+            get { return lastname; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException();
+                }
+                lastname = value;
+            }
+        }
+
+       
+
+    }
+}
