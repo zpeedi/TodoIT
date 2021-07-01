e1175a00998acb38863b3b912ec16c2c51764fb2
Magnus Larsson <zpeedi@hotmail.com>
2021-07-01 17:02:06 +0200

    Commit 4 Punkt 7 i uppgiften.

---
+++ TodoIT.Tests/TodoSequencerTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using TodoIT.Data;
+
+namespace TodoIT.Tests
+{
+    public class TodoSequencerTest
+    {
+        [Fact]
+        public void resetTest()
+        {
+            TodoSequencer.nextTodoSequencerId();
+            TodoSequencer.nextTodoSequencerId();
+            TodoSequencer.reset();
+
+            Assert.Equal(0, TodoSequencer.TodoSequencerId);
+        }
+
+        [Fact]
+        public void nextPersoIdTest()
+        {
+            int before = TodoSequencer.TodoSequencerId;
+            TodoSequencer.nextTodoSequencerId();
+
+            Assert.True(before < TodoSequencer.TodoSequencerId);
+        }
+    }
+}
---
+++ TodoIT/Data/TodoSequencer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TodoIT.Data
+{
+    public class TodoSequencer
+    {
+        private static int todoSequencerId;
+
+        public static int TodoSequencerId
+        {
+            get { return todoSequencerId; }
+        }
+        public static int nextTodoSequencerId()
+        {
+            return ++todoSequencerId;
+        }
+
+        public static void reset()
+        {
+            todoSequencerId = 0;
+        }
+    }
+}
