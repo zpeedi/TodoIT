3c537f7b291081f5f7b7ba03fec498c5bb2453f7
Magnus Larsson <zpeedi@hotmail.com>
2021-07-04 15:17:31 +0200

    Punkt 9.

---
+++ TodoIT.Tests/TodiItemTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using TodoIT.Model;
+using TodoIT.Data;
+
+
+namespace TodoIT.Tests
+{
+    public class TodiItemTests
+    {
+        [Fact]
+        public void NewTodoTest()
+        {
+
+            string description = "Kul grej";
+            Todo t = new Todo(TodoSequencer.nextTodoSequencerId(), description);
+            Person p = People.FindAll()[People.Size() - 1];
+
+            Assert.Equal(t.Description, description);          
+        }
+
+        [Fact]
+        public void FindByIdTest()
+        {
+
+            string description = "Kul grej";
+            int todoId = 1;
+
+            TodoItems.NewTodo(description);
+            
+            Todo t = TodoItems.FindById(todoId);
+
+            Assert.Equal(todoId, t.TodoId);
+
+
+        }
+    }
+}
---
+++ TodoIT/Data/TodoItems.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TodoIT.Model;
+
+namespace TodoIT.Data
+{
+    public class TodoItems 
+    {
+        private static Todo[] todoArray = new Todo[0];
+
+
+        public static int Size()
+        {
+            return todoArray.Length;
+        }
+
+        public static Todo[] FindAll()
+        {
+            return todoArray;
+        }
+
+        public static Todo FindById(int todoId)
+        {
+            foreach (Todo t in todoArray)
+            {
+                if (t.TodoId == todoId)
+                {
+                    return t;
+                }
+
+            }
+            return null;
+        }
+
+        public static Todo NewTodo(string description)
+        {
+            Todo t = new Todo(TodoSequencer.TodoSequencerId, description);
+            Array.Resize<Todo>(ref todoArray, todoArray.Length + 1);
+            todoArray[todoArray.Length - 1] = t;
+            return t;
+        }
+
+        public static void Clear()
+        {
+            Array.Clear(todoArray, 0, todoArray.Length);
+        }
+    }
+}
